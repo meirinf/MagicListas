d4a5ed1806c9b21f3f0c0181e3bdf5c28374b69c
Mireia <meirin.f@gmail.com>
2016-10-30 18:30:52 +0100

    Commit con todos los cambios de los fragments, eliminacion del boton, y nuevo layout para la lista de cartas

--- app/src/main/java/test/magiclistas/MainActivityFragment.java
+++ app/src/main/java/test/magiclistas/MainActivityFragment.java
@@ -5,6 +5,7 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ListView;
 
 /**
  * A placeholder fragment containing a simple view.
@@ -14,9 +15,12 @@ public class MainActivityFragment extends Fragment {
     public MainActivityFragment() {
     }
 
+    //List view con todas las cartas
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_main, container, false);
+        View view = inflater.inflate(R.layout.fragment_main, container, false);
+        ListView Cartas = (ListView) view.findViewById(R.id.Cartas);
+        return view;
     }
 }
