2bede3b078ca8b19766e330d37537ee69e2c8269
Mireia <meirin.f@gmail.com>
2016-12-05 16:09:19 +0100

    data binding inplementado

--- app/src/main/java/test/magiclistas/CartasAdapter.java
+++ app/src/main/java/test/magiclistas/CartasAdapter.java
@@ -1,13 +1,16 @@
 package test.magiclistas;
         import android.content.Context;
-        import android.databinding.DataBindingUtil;
-        import android.view.LayoutInflater;
-        import android.view.View;
-        import android.view.ViewGroup;
-        import android.widget.ArrayAdapter;
-        import com.bumptech.glide.Glide;
-        import java.util.List;
+import android.databinding.DataBindingUtil;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
 
+import com.bumptech.glide.Glide;
+
+import java.util.List;
+
+        import test.magiclistas.databinding.AdapterCartasBinding;
 
 
 /**
@@ -16,6 +19,8 @@ package test.magiclistas;
 
 public class CartasAdapter extends ArrayAdapter<Carta> {
 
+
+
     public CartasAdapter(Context context, int resource, List<Carta> objects) {
         super(context, resource, objects);
     }
@@ -23,16 +28,19 @@ public class CartasAdapter extends ArrayAdapter<Carta> {
     @Override
     public View getView(int position, View convertView, ViewGroup parent) {
 
+
         // Obtenim l'objecte en la possició corresponent
         Carta carta = getItem(position);
 
+        AdapterCartasBinding binding;
+        binding = null;
 
 
         // Mirem a veure si la View s'està reusant, si no es així "inflem" la View
-        content_details binding = null;
+
         if (convertView == null) {
             LayoutInflater inflater = LayoutInflater.from(getContext());
-            binding = DataBindingUtil.inflate(inflater, R.layout.content_details,parent,false);
+            binding = DataBindingUtil.inflate(inflater, R.layout.adapter_cartas,parent,false);
         } else binding = DataBindingUtil.getBinding(convertView);
 
         /*// Unim el codi en les Views del Layout
@@ -45,14 +53,14 @@ public class CartasAdapter extends ArrayAdapter<Carta> {
 
         // Fiquem les dades dels objectes (provinents del JSON) en el layout
 
-        binding.nombreCarta.setText("Nombre Carta: "+carta.getNombre());
-        binding.tipo.setText("Tipo: "+carta.getTipo());
-        binding.color.setText("Color: "+carta.getColor());
+        binding.adapterNombre.setText("Nombre Carta: "+carta.getNombre());
+        binding.adapterTipo.setText("Tipo: "+carta.getTipo());
+        binding.adapterColor.setText("Color: "+carta.getColor());
 
         //Imagen Carta
         Glide.with(getContext()).
                 load(carta.getImagen()).
-                into(binding.imagenCarta);
+                into(binding.adapterCarta);
 
         // Retornem la View replena per a mostrarla
         return binding.getRoot();
